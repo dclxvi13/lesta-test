5c4bf35889e769fbdaa84d1201a53c0a9586a2b9
Sergey Chukhnin <49841624+SmaugTheTerrible@users.noreply.github.com>
2024-05-18 18:54:58 +0500

    Implement storage through session scoped fixture

--- 01_pytest/conftest.py
+++ 01_pytest/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 
 from storage import storage
+from websiteinfo import get_website_data
 
 
+# Реализация хранения данных в фиксутре
+@pytest.fixture(scope="session")
+def website_data_scoped():
+    return get_website_data()
+
+
+# Реализация хранения данных в отдельном хранилище
 @pytest.fixture
 def website_data():
     return storage.website_data
